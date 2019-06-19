df9efd8d150af3e9776d4c63b5bfa19bcba70944
Eric Storey <estorey@gmail.com>
2019-06-19 15:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,19 @@ function exactMatch(drivers, object){
 }
 
 
-function driversWithRevenueOver(drivers, revenue){
-  return drivers.filter(function (driver){
-    return driver.revenue > revenue
+function exactMatchToList(drivers, object){
+  const driversOver= drivers.filter(function (driver){
+    let matched = false
+
+    for (const key in object) {
+      matched = driver[key] === object[key]
+    }
+
+    return matched
+  })
+  
+  return driversOver.map(function (driver){
+    return driver.name
   })
 }
 
